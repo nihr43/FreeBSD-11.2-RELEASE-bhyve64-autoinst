bbfedc99d7365ced22c9d9cfcde88861b400f1a1
nihr43 <nihr43@mail.missouri.edu>
2018-07-26 10:15:39 -0500

    kjdf

--- patch.sh
+++ patch.sh
@@ -2,33 +2,33 @@
 
 pkg install -y cdrtools ca_root_nss rsync
 
-if [ ! -e FreeBSD-11.1-RELEASE-amd64-disc1.iso ]
+if [ ! -e FreeBSD-11.2-RELEASE-amd64-disc1.iso ]
 then
- fetch https://download.freebsd.org/ftp/releases/amd64/amd64/ISO-IMAGES/11.1/FreeBSD-11.1-RELEASE-amd64-disc1.iso.xz
- xz -d ./FreeBSD-11.1-RELEASE-amd64-disc1.iso.xz
+ fetch https://download.freebsd.org/ftp/releases/amd64/amd64/ISO-IMAGES/11.2/FreeBSD-11.2-RELEASE-amd64-disc1.iso.xz
+ xz -d ./FreeBSD-11.2-RELEASE-amd64-disc1.iso.xz
 fi
 
 
-if [ -e ./FreeBSD-11.1-RELEASE-bhyve64-autoinst.iso ]
+if [ -e ./FreeBSD-11.2-RELEASE-bhyve64-autoinst.iso ]
 then
- rm ./FreeBSD-11.1-RELEASE-bhyve64-autoinst.iso
+ rm ./FreeBSD-11.2-RELEASE-bhyve64-autoinst.iso
 fi
 
 
-PATCHED_IMG_DIR=`mktemp -d`
-ORIG_IMG_DIR=`mktemp -d`
+PATCHED_ISO_DIR=`mktemp -d`
+ORIG_ISO_DIR=`mktemp -d`
 
-mdconfig -f ./FreeBSD-11.2-RC3-amd64-memstick.img -u 99
-mount /dev/md1s2a $ORIG_IMG_DIR
 
-rsync -aq $ORIG_IMG_DIR/ $PATCHED_IMG_DIR/
+mount -t cd9660 /dev/`mdconfig -f FreeBSD-11.2-RELEASE-amd64-disc1.iso` $ORIG_ISO_DIR
+rsync -aq $ORIG_ISO_DIR/ $PATCHED_ISO_DIR/
 
 # make modifications
-cp ./installerconfig $PATCHED_IMG_DIR/etc/installerconfig
-cp ./rc.local $PATCHED_IMG_DIR/etc/rc.local
+cp ./installerconfig $PATCHED_ISO_DIR/etc/installerconfig
+cp ./rc.local $PATCHED_ISO_DIR/etc/rc.local
 
-
-
+# create the new ISO.   VOLD_ID is important..
+VOL_ID=$(isoinfo -d -i FreeBSD-11.2-RELEASE-amd64-disc1.iso | grep "Volume id" | awk '{print $3}')
+mkisofs -J -R -no-emul-boot -V "$VOL_ID" -b boot/cdboot -o FreeBSD-11.2-RELEASE-bhyve64-autoinst.iso $PATCHED_ISO_DIR
 
 umount $ORIG_ISO_DIR
 rm -rf $ORIG_ISO_DIR
